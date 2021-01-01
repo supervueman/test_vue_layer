959d323065cc56c53fdd18307ea7397fccf6dc3d
supervueman <supervueman@gmail.com>
2021-01-01 15:56:27 +0400

    Add vuelayers

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import VueLayers from 'vuelayers'
+import 'vuelayers/lib/style.css' // needs css-loader
 
 Vue.config.productionTip = false;
+
+Vue.use(VueLayers)
 
 new Vue({
   router,
